d20a4531925b47f46e827628048155fc08ff5106
aersosi <aersosi@avodaq.com>
2025-01-17 20:55:57 +0100

    [feat] stuff

--- src/core/features/remove_ElementsOnLoad.js
+++ src/core/features/remove_ElementsOnLoad.js
@@ -1,7 +1,7 @@
-export function remove_ElementsOnLoad(idArrary) {
+export function remove_ElementsOnLoad(idArrary, logConsole = false) {
   idArrary.forEach(id => {
     const element = document.getElementById(id);
-    !element && console.warn(`Element with ID "${id}" not found.`);
+    logConsole && !element && console.warn(`Element with ID "${id}" not found.`);
 
     element && element.remove();
   })
--- todo.md
+++
@@ -1,5 +0,0 @@
-# Todo
-
-- Add tooltips https://kazzkiq.github.io/balloon.css/
-- check https://www.okcupid.com/profile console on scroll
-- check https://www.okcupid.com/who-likes-you console: clearInterval broken
