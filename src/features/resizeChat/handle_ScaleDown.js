edd35a2d85fa49b26b7327a8a9e49783fee4461c
aersosi <aersosi@avodaq.com>
2025-02-02 20:32:22 +0100

    [feat] add resize chat window

---
+++ src/features/resizeChat/handle_ScaleDown.js
@@ -0,0 +1,5 @@
+import { remove_elementsWithID } from "../../core/index.js";
+
+export function handle_scaleDown() {
+  remove_elementsWithID(["resize_okc_messangerWindow"]);
+}
--- src/index.js
+++ src/index.js
@@ -10,17 +10,19 @@ import { censor_okc } from "../dist_styles/censor_okc.js";
 import { okc_overrides } from "../dist_styles/okc_overrides.js";
 import { okc_superlikeBtn } from "../dist_styles/okc_superlikeBtn.js";
 import { okc_userThumb } from "../dist_styles/okc_userThumb.js";
-import { dr_variables } from "../dist_styles/dr_variables.js";
-import { dr_elements } from "../dist_styles/dr_elements.js";
-import { dr_UI_sections } from "../dist_styles/dr_UI_sections.js";
-import { dr_UI } from "../dist_styles/dr_UI.js";
-import { dr_helper } from "../dist_styles/dr_helper.js";
 import { dr_blockBtn } from "../dist_styles/dr_blockBtn.js";
+
+import { dr_variables } from "../dist_styles/dr_variables.js";
+import { dr_helper } from "../dist_styles/dr_helper.js";
+import { dr_UI } from "../dist_styles/dr_UI.js";
+import { dr_UI_sections_whoYouLike } from "../dist_styles/dr_UI_sections_whoYouLike.js";
+import { dr_components } from "../dist_styles/dr_components.js";
 
 import { styles_pageDiscover } from "../dist_styles/styles_pageDiscover.js";
 import { styles_pageMessages } from "../dist_styles/styles_pageMessages.js";
 import { styles_pageProfile } from "../dist_styles/styles_pageProfile.js";
 import { styles_pageWhoYouLike } from "../dist_styles/styles_pageWhoYouLike.js";
+import { init_resizeChat } from "./features/resizeChat/init_resizeChat.js";
 
 (async function() {
   "use strict";
@@ -31,12 +33,15 @@ import { styles_pageWhoYouLike } from "../dist_styles/styles_pageWhoYouLike.js";
       before_document_interactive: [
         () => remove_elementsWithID(["dr_UI_wrapper", "okc_superlikeBtn", "okc_userThumb"]),
 
-        () => inject_stylesToHead(okc_overrides, "okc_overrides"),
         () => inject_stylesToHead(dr_variables, "dr_variables"),
-        () => inject_stylesToHead(dr_elements, "dr_elements"),
-        () => inject_stylesToHead(dr_UI, "dr_UI"),
         () => inject_stylesToHead(dr_helper, "dr_helper"),
-        () => inject_stylesToHead(censor_okc, "censor_okc")
+        () => inject_stylesToHead(dr_UI, "dr_UI"),
+        () => inject_stylesToHead(dr_components, "dr_components"),
+      ],
+      document_interactive: [
+        () => inject_stylesToHead(okc_overrides, "okc_overrides"),
+        () => inject_stylesToHead(censor_okc, "censor_okc"),
+        () => init_resizeChat(),
       ]
     }
   );
@@ -91,9 +96,9 @@ import { styles_pageWhoYouLike } from "../dist_styles/styles_pageWhoYouLike.js";
       waitForElement: ".userrow-bucket-container",
       document_interactive: [
         () => inject_scriptToHead(inject_storeUserData, "inject_storeUserData"),
-        () => inject_stylesToHead(dr_UI_sections, "dr_UI_sections"),
         () => inject_stylesToHead(okc_superlikeBtn, "okc_superlikeBtn"),
         () => inject_stylesToHead(okc_userThumb, "okc_userThumb"),
+        () => inject_stylesToHead(dr_UI_sections_whoYouLike, "dr_UI_sections_whoYouLike"),
         () => inject_stylesToHead(styles_pageWhoYouLike, "styles_pageWhoYouLike")
       ],
       document_complete: [
