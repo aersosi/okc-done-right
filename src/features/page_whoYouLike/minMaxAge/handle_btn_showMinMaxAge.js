92d4ceb92d3d4730a06aa5e48e8407354362f9f2
aersosi <aersosi@avodaq.com>
2025-02-21 22:01:22 +0100

    [improve] init minMaxAge

--- src/features/page_whoYouLike/minMaxAge/handle_btn_showMinMaxAge.js
+++ src/features/page_whoYouLike/minMaxAge/handle_btn_showMinMaxAge.js
@@ -1,7 +1,7 @@
 import { set_minMaxAge } from "./set_minMaxAge.js";
 
 export function handle_btn_showMinMaxAge() {
-  const storedValue = JSON.parse(localStorage.getItem("dr_minMaxAge")) || { minAge: 18, maxAge: 99 };
+  const storedValue = JSON.parse(localStorage.getItem("dr_minMaxAge"));
   storedValue.areHidden = false;
   localStorage.setItem("dr_minMaxAge", JSON.stringify(storedValue));
 
--- src/features/page_whoYouLike/minMaxAge/init_minMaxAge.js
+++ src/features/page_whoYouLike/minMaxAge/init_minMaxAge.js
@@ -1,6 +1,10 @@
 export function init_minMaxAge() {
   const storedValue = localStorage.getItem("dr_minMaxAge");
-  if (storedValue === null) {
+  if (storedValue) {
+    const newValue = JSON.parse(storedValue);
+    const newState = { areHidden: false, minAge: newValue.minAge, maxAge: newValue.maxAge };
+    localStorage.setItem("dr_minMaxAge", JSON.stringify(newState));
+  } else {
     let minMaxAgeValue = { areHidden: false, minAge: 18, maxAge: 99 };
     localStorage.setItem("dr_minMaxAge", JSON.stringify(minMaxAgeValue));
   }
